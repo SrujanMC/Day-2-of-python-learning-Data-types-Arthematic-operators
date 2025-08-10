37c539157e6113d201c96458dfc0676112404c97
SrujanMC <srujanmc2009@gmail.com>
2025-08-10 21:23:56 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,27 @@
+#Day 1
+Myname="Srujan"
+length=5
+width=6
+w=width
+l=length
+N=Myname
+print("My name is:",N)
+print("Perimeter of rectangle is:",2*(w+l))
+print("area of rectangle:",l*w)
+#Day 2
+a=8
+b=10
+c=True
+d=False
+e=None
+print("sum is:",a+b)
+print("Difference is:",a-b)
+print("multiplication is:",a*b)
+print("division is:",a/b)
+print("power is:",a**b)
+print("reminder is:",a%b)
+print(type(a))
+print(type(N))
+print(type(b))
+print(type(c))
+print(type(e))
